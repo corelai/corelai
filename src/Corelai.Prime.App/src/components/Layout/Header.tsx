cd92a8186dfe539edc340e0f0e55acebd0e77be3
dustech <stefano.cerruti@outlook.com>
2025-04-13 15:39:23 +0200

    Better header positioning

--- src/Corelai.Prime.App/src/components/Layout/Header.tsx
+++ src/Corelai.Prime.App/src/components/Layout/Header.tsx
@@ -3,8 +3,8 @@ import img from "/corelai-logo.svg";
 
 const Header: React.FC = () => {
     return (
-        <div className="flex bg-secondary-surface justify-center items-center sm:gap-8 pb-6 sm:pb-8 border-b-4 sm:border-b-6 md:border-b-8 golden-border">
-            <div className="text-center  bg-secondary-surface ">
+        <div className="flex bg-secondary-surface ps-8 sm:ps-12 md:ps-16 justify-left items-center sm:gap-8 pb-6 sm:pb-8 border-b-4 sm:border-b-6 md:border-b-8 golden-border">
+            <div className="  bg-secondary-surface ">
                 <div className="flex flex-row">
                     <div className="text-white font-orbitron text-4xl uppercase   whitespace-nowrap">
                         C
