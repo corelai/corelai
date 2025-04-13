17ef2d7475bfccbb7feb3cf69fe8204a0a051b21
dustech <stefano.cerruti@outlook.com>
2025-04-13 18:52:37 +0200

    Better footer

--- src/Corelai.Prime.App/src/components/Layout/Footer.tsx
+++ src/Corelai.Prime.App/src/components/Layout/Footer.tsx
@@ -9,14 +9,15 @@ const Footer: React.FC = () => {
     return (
         <footer className="bg-primary-surface text-primary text-center font-orbitron pt-4 min-h-16 sm:min-h-24
         ">
-            <section className="text-sm tracking-wide flex flex-col items-center justify-center gap-2">
-                <div>© {currentYear} Corelai SECTOR</div>
-                <div>Glory to the Build. Control to Corelai.</div>
-                <div className="text-xs opacity-60">Terraforming Authorization: Level 1</div>
+            <section className="text-sm tracking-wide flex flex-col sm:flex-row
+                            items-center justify-center sm:justify-start
+                             gap-2 pb-2 sm:px-12">
+                <div >© {currentYear} Corelai SECTOR</div>
+                <div className="sm:ms-auto opacity-50 text-xs">Glory to the Build. Control to Corelai.</div>
             </section>
 
             <section
-                className="text-xs text-slate-600 flex items-center justify-end gap-2 align-middle p-4 pt-8 bg-slate-950">
+                className="text-xs text-slate-600 flex items-center justify-end gap-2 align-middle p-2 pt-6 bg-slate-950">
                 Corelai Repo -
                 <a href="https://github.com/corelai" target="_blank" rel="noopener noreferrer">
                     <FaGithub className="w-4 h-4 sm:w-6 sm:h-6 hover:text-secondary transition-colors"/>
