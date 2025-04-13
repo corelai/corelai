818852377e6dc00aaee69a5fc1da6b9e8410d468
dustech <stefano.cerruti@outlook.com>
2025-04-13 18:55:11 +0200

    Better sidebar

--- src/Corelai.Prime.App/src/components/Layout/Sidebar.tsx
+++ src/Corelai.Prime.App/src/components/Layout/Sidebar.tsx
@@ -3,7 +3,7 @@ import {Symbol} from "./Symbol.tsx";
 
 const Sidebar: React.FC = () => {
     return (
-        <div className="flex justify-center h-full bg-secondary-surface text-white p-4">
+        <div className="flex justify-center h-full bg-secondary-surface text-white p-4 border-e-1 border-primary">
             <div className="flex h-full max-w-3/4">
                 <Symbol/>
             </div>
