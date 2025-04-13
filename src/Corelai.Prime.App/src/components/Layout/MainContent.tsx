4fd92dceb9c036e873b66e6a2a4a0a49eee8a48b
dustech <stefano.cerruti@outlook.com>
2025-04-13 16:35:05 +0200

    Better welcome

--- src/Corelai.Prime.App/src/components/Layout/MainContent.tsx
+++ src/Corelai.Prime.App/src/components/Layout/MainContent.tsx
@@ -20,13 +20,14 @@ const CORELAI_SLOGAN = `
 const MainContent: React.FC = () => {
     return (
         <div className="text-center" >
-            <h1 className="font-[700]">
+            <h1 className="font-[700] py-2 sm:py-4">
                 Welcome to Corelai Prime
             </h1>
-            <p className="text-start px-8 pb-8 text-xs font-text whitespace-pre-line">
+            <p className="text-start px-8 text-xs font-text whitespace-pre-line">
                 {welcomeMessage}
-                <span className="font-orbitron">{CORELAI_SLOGAN}</span>
+
             </p>
+            <p className="py-4 sm:py-8 font-orbitron text-sm text-center">{CORELAI_SLOGAN}</p>
             <h3 className="font-[600]">
                 h3 text
             </h3>
