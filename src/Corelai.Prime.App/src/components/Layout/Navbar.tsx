1a78022b912ec6038c5f1ee973cb6dc1e90105db
dustech <stefano.cerruti@outlook.com>
2025-04-16 15:50:30 +0200

    Little corner

--- src/Corelai.Prime.App/src/components/Layout/Navbar.tsx
+++ src/Corelai.Prime.App/src/components/Layout/Navbar.tsx
@@ -4,6 +4,7 @@ import {ThemeToggleButton} from "../ThemeToggleButton/ThemeToggleButton.tsx";
 const Navbar: React.FC = () => {
     return (
         <div className="bg-secondary-surface text-secondary flex justify-end py-2 pe-2">
+            <p className="pe-2 font-encode uppercase text-xs">Colony level 1</p>
             <ThemeToggleButton/>
         </div>
     );
