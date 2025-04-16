9c032dfe912ceda451c497d0ebe841a49ee3175b
dustech <stefano.cerruti@outlook.com>
2025-04-16 16:58:32 +0200

    BEtter logo

--- src/Corelai.Prime.App/src/components/Layout/CorelaiLogo.tsx
+++ src/Corelai.Prime.App/src/components/Layout/CorelaiLogo.tsx
@@ -3,40 +3,53 @@ import {CorelaiLaurel} from "../UI/CorelaiLaurel.tsx";
 
 
 export const CorelaiLogo: React.FC = () => {
-    return (<div className="relative ">
-        <a href="/">
-            <span className="absolute inset-0"></span>
-        </a>
-        <div className="flex flex-row">
+    return (
+        <div className="relative
+        py-3
+        ">
+            <a href="/" className="">
+                <span className="absolute inset-0"></span>
+            </a>
             <div className="
-                translate-x-0.5
-                sm:translate-x-1
-
-                size-8 pt-0.5
-                sm:size-13
-                md:size-12 md:pt-0.5">
-                <CorelaiLaurel className="size-full text-primary"/>
-            </div>
-            <div className="text-primary/80 font-orbitron
-            pt-3
-            sm:pt-5
-            text-lg
-            sm:text-4xl
-            uppercase
-            whitespace-nowrap">
-                orelai
-            </div>
-            <div
-                className="text-division  font-oxanium font-[400] uppercase
+                flex
+            flex-row
+            sm:flex-col
+            justify-evenly
+            sm:justify-start
+            sm:items-center
+            ">
+                <div className="flex flex-row">
+                    <div className="
+                        size-8 pt-0.5
+                        sm:size-13
+                    ">
+                        <CorelaiLaurel className="size-full text-primary"/>
+                    </div>
+                    <div className="text-primary/65 font-orbitron tracking-widest
+                                    pt-3
+                                    sm:pt-5
+                                    text-lg
+                                    sm:text-4xl
+                                    uppercase
+                                    whitespace-nowrap">
+                        orelai
+                    </div>
+                </div>
+                <div
+                    className="text-division  font-oxanium font-[400] uppercase
+                self-end
+                sm:self-auto
                 text-xs
                 sm:text-sm
                 md:text-lg
 
-                md:text-blue-500
-                -translate-x-11 translate-y-8
-                sm:-translate-x-13 sm:translate-y-13">
-                sector
+                {/*-translate-x-11 translate-y-8*/}
+                {/*sm:-translate-x-13 sm:translate-y-13*/}
+                {/*md:-translate-x-16 */}
+                ">
+                    sector // prime
+                </div>
             </div>
         </div>
-    </div>);
+    );
 }
--- src/Corelai.Prime.App/src/components/Layout/Header.tsx
+++ src/Corelai.Prime.App/src/components/Layout/Header.tsx
@@ -4,20 +4,10 @@ import {CorelaiLogo} from "./CorelaiLogo.tsx";
 const Header: React.FC = () => {
     return (
         <div
-            className="flex bg-primary-surface
-            ps-8
-            sm:ps-10
-            md:ps-16
-            py-6
-            sm:py-8
-            justify-left items-center
+            className="bg-primary-surface
              border-b-4 sm:border-b-6 md:border-b-8 golden-border">
             <CorelaiLogo/>
-            <div className="flex sm:justify-center w-full">
-                <div className="text-lg sm:text-4xl font-orbitron text-primary uppercase text-nowrap ">
-                    // prime \\
-                </div>
-            </div>
+
         </div>
     );
 };
