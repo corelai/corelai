e8dfb3988f9657b529e9947ed1844560691bf553
dustech <stefano.cerruti@outlook.com>
2025-04-16 14:21:47 +0200

    Light theme ok

--- src/Corelai.Prime.App/src/components/Layout/Footer.tsx
+++ src/Corelai.Prime.App/src/components/Layout/Footer.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import {FaGithub} from 'react-icons/fa' // usa react-icons per semplicità
-import tailwindLogo from '/images/tailwindcss-mark.svg'
-import reactLogo from '/images/react.svg'
+import {TechStack} from "../TechStack/TechStack.tsx";
 
 const Footer: React.FC = () => {
     const currentYear = new Date().getFullYear()
@@ -16,25 +14,7 @@ const Footer: React.FC = () => {
                 <div className="sm:ms-auto opacity-50 text-xs">Glory to the Build. Control to Corelai.</div>
             </section>
 
-            <section
-                className="text-xs text-slate-600 flex items-center justify-end gap-2 align-middle p-2 pt-6 bg-slate-950">
-                Corelai Repo -
-                <a href="https://github.com/corelai" target="_blank" rel="noopener noreferrer">
-                    <FaGithub className="w-4 h-4 sm:w-6 sm:h-6 hover:text-secondary transition-colors"/>
-                </a>
-                <div>Powered by</div>
-                <div className="h-4 w-4 sm:w-6 sm:h-6">
-                    <a href="https://react.dev/" target="_blank" rel="noopener noreferrer">
-                        <img src={reactLogo} alt="React"/>
-                    </a>
-                </div>
-                <div>&</div>
-                <div className="h-4 w-4 sm:w-6 sm:h-6 pt-1">
-                    <a href="https://tailwindcss.com/" target="_blank" rel="noopener noreferrer">
-                        <img src={tailwindLogo} alt="Tailwind" className="h-4"/>
-                    </a>
-                </div>
-            </section>
+            <TechStack/>
         </footer>
     )
 }
--- src/Corelai.Prime.App/src/components/Layout/Sidebar.tsx
+++ src/Corelai.Prime.App/src/components/Layout/Sidebar.tsx
@@ -3,7 +3,7 @@ import {CorelaiLaurel} from "../UI/CorelaiLaurel.tsx";
 
 const Sidebar: React.FC = () => {
     return (
-        <div className="flex justify-center h-full bg-secondary-surface  p-4 border-e-1 border-primary">
+        <div className="flex justify-center h-full bg-secondary-surface  p-4 border-e-1 border-division-surface">
             <div className="flex h-full max-w-3/4">
                 <CorelaiLaurel className="size-full text-division"/>
             </div>
