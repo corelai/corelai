e9758252080baf0e52ef177642f5ee22e0485929
Stefano Cerruti <stefano.cerruti@outlook.com>
2025-04-16 19:26:59 +0200

    Better header (#26)

--- src/Corelai.Prime.App/src/components/Layout/Header.tsx
+++ src/Corelai.Prime.App/src/components/Layout/Header.tsx
@@ -5,7 +5,9 @@ const Header: React.FC = () => {
     return (
         <div
             className="bg-primary-surface
-             border-t-4 sm:border-t-6 md:border-t-8 golden-border">
+             border-t-4 sm:border-t-6 md:border-t-8 golden-border
+             flex justify-center
+             ">
             <CorelaiLogo/>
 
         </div>
