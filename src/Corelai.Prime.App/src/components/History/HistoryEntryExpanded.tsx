5955f9da127079a2e6cd2d6b6a333a39eb62341b
dustech <stefano.cerruti@outlook.com>
2025-04-17 23:19:14 +0200

    Working on history

---
+++ src/Corelai.Prime.App/src/components/History/HistoryEntryExpanded.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+export const HistoryEntryExpanded: React.FC = () => {
+    return (
+        <>
+
+            <div className="
+                sm:rounded-4xl
+                bg-surface-50
+                shadow-sm shadow-surface-400
+                dark:shadow-none
+                max-w-[499px]
+                overflow-hidden
+                ">
+
+                <div className="h-48 bg-[url(/images/history/CL-PRM-HIST-0001.png)] bg-center bg-cover bg-no-repeat"></div>
+
+                <h1 className="uppercase tracking-wide font-[400] bg-surface-50">h1 400 test for the win</h1>
+                <h1 className="uppercase tracking-wide font-[600] bg-surface-50">h1 600 test for the win</h1>
+                <h1 className="uppercase tracking-wide font-[700] bg-surface-50">h1 700 test for the win</h1>
+                <div className="py-4 bg-surface-50"></div>
+                <div className="hyphens-auto indent-4 font-[400] text-base bg-surface-50"> Font 200.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Accusantium adipisci architecto blanditiis, corporis dolor eos labore laudantium maiores nisi non
+                    odit
+                    perspiciatis, quae
+                    quaerat repellat sed soluta veritatis vitae voluptatem?
+
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam cumque est iure
+                    maxime minima molestias non perferendis saepe, velit!
+                </div>
+            </div>
+        </>
+    )
+}
--- src/Corelai.Prime.App/src/components/Layout/Layout.tsx
+++ src/Corelai.Prime.App/src/components/Layout/Layout.tsx
@@ -4,49 +4,35 @@ import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
 import '../../styles/layout.css';
+import {HistoryEntryExpanded} from "../History/HistoryEntryExpanded.tsx";
+
 
 const Layout: React.FC = () => {
     return (
         <>
-            <div className="w-full h-full flex justify-center items-center">
-
-                <div className="bg-surface-50 p-8
-                shadow-sm shadow-surface-400
-                dark:shadow-none
-                ">
-                    <h1 className="uppercase tracking-wide font-[400]">h1 400 test for the win</h1>
-                    <h1 className="uppercase tracking-wide font-[600]">h1 600 test for the win</h1>
-                    <h1 className="uppercase tracking-wide font-[700]">h1 700 test for the win</h1>
-                    <div className="py-4"></div>
-                    <div className="hyphens-auto indent-4 font-[400] text-base"> Font 200.
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                        Accusantium adipisci architecto blanditiis, corporis dolor eos labore laudantium maiores nisi non odit perspiciatis, quae
-                        quaerat repellat sed soluta veritatis vitae voluptatem?
-
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam cumque est iure
-                        maxime minima molestias non perferendis saepe, velit!
-                    </div>
-                </div>
-
+            <div className="w-full h-full flex-col justify-center items-center
+            sm:p-8 md:p-16 lg:p-32
+            ">
+                <HistoryEntryExpanded/>
 
             </div>
-            <div className="grid grid-areas-layout grid-cols-4 grid-rows-[auto_auto_1fr_auto] min-h-dvh ">
-                <nav className="grid-in-nav col-span-4">
-                    <Navbar/>
-                </nav>
-                <header className="grid-in-header col-span-4">
-                    <Header/>
-                </header>
-                {/*<aside className="grid-in-side hidden sm:hidden">*/}
-                {/*    <Sidebar />*/}
-                {/*</aside>*/}
-                <main className="grid-in-main col-span-4 sm:col-span-4">
-                    <MainContent/>
-                </main>
-                <footer className="grid-in-footer col-span-4">
-                    <Footer/>
-                </footer>
-            </div>
+            {/*<div className="grid grid-areas-layout grid-cols-4 grid-rows-[auto_auto_1fr_auto] min-h-dvh ">*/}
+            {/*    <nav className="grid-in-nav col-span-4">*/}
+            {/*        <Navbar/>*/}
+            {/*    </nav>*/}
+            {/*    <header className="grid-in-header col-span-4">*/}
+            {/*        <Header/>*/}
+            {/*    </header>*/}
+            {/*    /!*<aside className="grid-in-side hidden sm:hidden">*!/*/}
+            {/*    /!*    <Sidebar />*!/*/}
+            {/*    /!*</aside>*!/*/}
+            {/*    <main className="grid-in-main col-span-4 sm:col-span-4">*/}
+            {/*        <MainContent/>*/}
+            {/*    </main>*/}
+            {/*    <footer className="grid-in-footer col-span-4">*/}
+            {/*        <Footer/>*/}
+            {/*    </footer>*/}
+            {/*</div>*/}
         </>
     );
 };
