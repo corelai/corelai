8bf879974c37c5f12d3fe661f377868be5346a44
dustech <stefano.cerruti@outlook.com>
2025-04-17 22:22:57 +0200

    Merge restart

--- src/Corelai.Prime.App/src/components/Layout/Layout.tsx
+++ src/Corelai.Prime.App/src/components/Layout/Layout.tsx
@@ -7,23 +7,46 @@ import '../../styles/layout.css';
 
 const Layout: React.FC = () => {
     return (
-        <div className="grid grid-areas-layout grid-cols-4 grid-rows-[auto_auto_1fr_auto] min-h-dvh ">
-            <nav className="grid-in-nav col-span-4">
-                <Navbar />
-            </nav>
-            <header className="grid-in-header col-span-4">
-                <Header />
-            </header>
-            {/*<aside className="grid-in-side hidden sm:hidden">*/}
-            {/*    <Sidebar />*/}
-            {/*</aside>*/}
-            <main className="grid-in-main col-span-4 sm:col-span-4">
-                <MainContent />
-            </main>
-            <footer className="grid-in-footer col-span-4">
-                <Footer />
-            </footer>
-        </div>
+        <>
+            <div className="w-full h-full flex justify-center items-center">
+
+                <div className="bg-surface-50 p-8 rounded-4xl
+                shadow-sm shadow-surface-400
+                ">
+                    <h1 className="uppercase tracking-wide font-[400]">h1 400 test for the win</h1>
+                    <h1 className="uppercase tracking-wide font-[600]">h1 600 test for the win</h1>
+                    <h1 className="uppercase tracking-wide font-[700]">h1 700 test for the win</h1>
+                    <div className="py-4"></div>
+                    <div className="hyphens-auto indent-4 font-[400] text-base"> Font 200.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Accusantium adipisci architecto blanditiis, corporis dolor eos labore laudantium maiores nisi non odit perspiciatis, quae
+                        quaerat repellat sed soluta veritatis vitae voluptatem?
+
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam cumque est iure
+                        maxime minima molestias non perferendis saepe, velit!
+                    </div>
+                </div>
+
+
+            </div>
+            <div className="grid grid-areas-layout grid-cols-4 grid-rows-[auto_auto_1fr_auto] min-h-dvh ">
+                <nav className="grid-in-nav col-span-4">
+                    <Navbar/>
+                </nav>
+                <header className="grid-in-header col-span-4">
+                    <Header/>
+                </header>
+                {/*<aside className="grid-in-side hidden sm:hidden">*/}
+                {/*    <Sidebar />*/}
+                {/*</aside>*/}
+                <main className="grid-in-main col-span-4 sm:col-span-4">
+                    <MainContent/>
+                </main>
+                <footer className="grid-in-footer col-span-4">
+                    <Footer/>
+                </footer>
+            </div>
+        </>
     );
 };
 
