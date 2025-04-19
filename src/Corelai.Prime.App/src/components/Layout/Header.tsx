ab2291686556305ed84d6df4382caaf20777dbe7
dustech <stefano.cerruti@outlook.com>
2025-04-19 14:21:14 +0200

    better layout setup

--- src/Corelai.Prime.App/src/components/Layout/Header.tsx
+++ src/Corelai.Prime.App/src/components/Layout/Header.tsx
@@ -1,47 +1,17 @@
 import React from 'react';
 import {CorelaiLogo} from "./CorelaiLogo.tsx";
-import {ThemeToggleButton} from "../ThemeToggleButton/ThemeToggleButton.tsx";
 
 
-const MediaSpy = () => {
-    return (
-        <div className={`fixed left-0`}>
-            <div className={`uppercase font-[600] h-4 text-[0.8rem] block sm:hidden w-[30rem]`}>Mobile 30rem</div>
-            <div className={`uppercase font-[600] h-4 text-[0.8rem] hidden sm:block md:hidden bg-red-400 w-[48rem]`}>SM 30-48rem 480px-768px</div>
-            <div className={`uppercase font-[600] h-4 text-[0.8rem] hidden md:block lg:hidden bg-amber-400 w-[64rem]`}>MD 48-64rem 768px-1024px</div>
-            <div className={`uppercase font-[600] h-4 text-[0.8rem] hidden lg:block xl:hidden bg-green-400 w-[80rem]`}>LG 64-80rem 1024px-1280px</div>
-            <div className={`uppercase font-[600] h-4 text-[0.8rem] hidden xl:block 2xl:hidden bg-sky-400 w-[96rem]`}>XL 80-96rem 1280px-1536px</div>
-            <div className={`uppercase font-[600] h-4 text-[0.8rem] hidden 2xl:block n bg-purple-400 w-screen`}>2XL &gt;96rem &gt;1536px</div>
-        </div>
-    )
-}
+
 
 const Header: React.FC = () => {
     return (
         <div
-            className="
-             border-t-4 sm:border-t-6 md:border-t-8 golden-border
+            className=" pt-16
+             border-t-4 sm:border-t-6 md:border-t-8
              flex justify-center
-             relative
              ">
-            <MediaSpy/>
             <CorelaiLogo/>
-            <ThemeToggleButton className="
-            size-10
-            rounded-full
-            transition-colors
-            text-surface-950
-            hover:text-gold-400
-            dark:text-surface-300
-            dark:hover:text-gold-400
-            text-center
-            self-center
-            absolute
-            right-2 sm:right-4
-            top-2 sm:top-4
-            "
-                               iconClassName="size-6 sm:size-12"
-            />
         </div>
     );
 };
--- src/Corelai.Prime.App/src/components/Layout/Layout.tsx
+++ src/Corelai.Prime.App/src/components/Layout/Layout.tsx
@@ -1,45 +1,41 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Header from './Header';
-import MainContent from './MainContent';
 import Footer from './Footer';
 import '../../styles/layout.css';
 import History from "../History/History.tsx";
+import {WelcomeCitizen} from "../WelcomeCitizen/WelcomeCitizen.tsx";
 
 
 const Layout: React.FC = () => {
     return (
         <>
-            <Header/>
-            <div className="py-4"></div>
-            <div className="w-full h-full flex-col justify-center items-center
-            py-16
-            sm:p-16
-            md:p-24
-            lg:p-32
-            ">
-                <div className="overflow-y-auto min-h-[75%] max-h-[75%] scrollbar-thin scrollbar-thumb-surface-400 scrollbar-track-transparent">
-                <History/>
+            <div className="grid grid-areas-layout grid-cols-4 grid-rows-[auto_auto_1fr_auto] min-h-dvh ">
+                <nav className="grid-in-nav col-span-4">
+                    <Navbar/>
+                </nav>
+                <header className="grid-in-header col-span-4">
+                    <Header/>
+                </header>
+                <main className="grid-in-main col-span-4 sm:col-span-4">
+                    <div className="py-4"></div>
+                    <WelcomeCitizen/>
+                    <div className="
+                            py-16
+                            sm:p-16
+                            md:p-24
+                            lg:p-32
+                            ">
+                        <div
+                            className="overflow-y-auto min-h-[75%] max-h-[75%] scrollbar-thin scrollbar-thumb-surface-400 scrollbar-track-transparent">
+                            <History/>
+                        </div>
                     </div>
-
+                </main>
+                <footer className="grid-in-footer col-span-4">
+                    <Footer/>
+                </footer>
             </div>
-            {/*<div className="grid grid-areas-layout grid-cols-4 grid-rows-[auto_auto_1fr_auto] min-h-dvh ">*/}
-            {/*    <nav className="grid-in-nav col-span-4">*/}
-            {/*        <Navbar/>*/}
-            {/*    </nav>*/}
-            {/*    <header className="grid-in-header col-span-4">*/}
-            {/*        <Header/>*/}
-            {/*    </header>*/}
-            {/*    /!*<aside className="grid-in-side hidden sm:hidden">*!/*/}
-            {/*    /!*    <Sidebar />*!/*/}
-            {/*    /!*</aside>*!/*/}
-            {/*    <main className="grid-in-main col-span-4 sm:col-span-4">*/}
-            {/*        <MainContent/>*/}
-            {/*    </main>*/}
-            {/*    <footer className="grid-in-footer col-span-4">*/}
-            {/*        <Footer/>*/}
-            {/*    </footer>*/}
-            {/*</div>*/}
         </>
     );
 };
