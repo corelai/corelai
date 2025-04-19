a1f8b035d478bd238946b69d1372def35044f97c
dustech <stefano.cerruti@outlook.com>
2025-04-19 15:00:24 +0200

    use montserrat

--- src/Corelai.Prime.App/src/components/Timeline/HistoryEntrySummary.tsx
+++ src/Corelai.Prime.App/src/components/Timeline/HistoryEntrySummary.tsx
@@ -1,14 +1,14 @@
 const HistoryEntrySummary = ({summary}: { summary: string }) =>
     (
-        <h2 className="tracking-wide
+        <div className="tracking-wide
                 text-sm
                 sm:text-base
                 font-[400] py-12
                 px-8 sm:px-16
                 dark:font-[600]
                 ">
-            {summary}
-        </h2>
+            {summary} dio
+        </div>
     )
 
 export default HistoryEntrySummary;
