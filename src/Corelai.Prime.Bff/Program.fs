b30d433882db68801ca4268c5603060dca1e3556
dustech <stefano.cerruti@outlook.com>
2025-04-24 02:31:26 +0200

    Add sample routing

--- src/Corelai.Prime.Bff/Program.fs
+++ src/Corelai.Prime.Bff/Program.fs
@@ -1,11 +1,14 @@
 namespace Corelai.Prime.Bff
 
+open Giraffe.TokenRouter
+open Giraffe.ViewEngine
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.Hosting
 open Railway
 open Giraffe
 open Timeline
 open ApiResponder
+open TokenRouter
 
 module Program =
     let exitCode = 0
@@ -17,14 +20,33 @@ module Program =
             return b * 10
         }
 
-    let webApp =
-        choose [ route "/test" >=> text "ok"
-                 route "/timeline/0000" >=> json ilionEvent
-                 route "/rail" >=> toHttp sampleApiCall
-                 route "/my-error"
-                 >=> toHttp (task { return Error "Bonzio error" })
-                 route "/my-not-found"
-                 >=> toHttp (task { return Ok None }) ]
+
+    let notFound = setStatusCode 404 >=> text "Not found"
+
+    let routing =
+        router
+            notFound
+            [ route "/" (text "index")
+              route "/test" (text "ok")
+              routef "parsing/%s/%i" (fun (s, i) -> text (sprintf "Received %s & %i" s i))
+              subRoute
+                  "/errors"
+                  [ GET [ route "/my-error" (toHttp (task { return Error "Bonzio error" }))
+                          route "/my-not-found" (toHttp (task { return Ok None }))
+                          subRoute "/v2" [ route "/my-not-found" (toHttp (task { return Ok None })) ] ] ]
+              subRoute
+                  "/oks"
+                  [ route "/test" (text "ok")
+                    route "/rail" (toHttp sampleApiCall) ]
+              route
+                  "/timelines/0000"
+                  (ror {
+                      let! a = task { return Ok(Some ilionEvent) }
+                      return a
+                   }
+                   |> toHttp)
+
+              ]
 
     [<EntryPoint>]
     let main args =
@@ -36,7 +58,7 @@ module Program =
 
         let app = builder.Build()
 
-        app.UseGiraffe(webApp)
+        app.UseGiraffe(routing)
 
         app.Run()
 
