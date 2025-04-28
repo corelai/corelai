022ef33e6ee1a11ca268a2e1708d63d76fec9cb2
dustech <stefano.cerruti@outlook.com>
2025-04-29 00:39:41 +0200

    Catch exceptions

--- src/Corelai.Prime.Bff/ApiResponder.fs
+++ src/Corelai.Prime.Bff/ApiResponder.fs
@@ -9,18 +9,23 @@ module ApiResponder =
 
     let toHttp (result: ApiResult<'T, string>) (ctx: HttpContext) : Task =
         task {
-            let! res = result
+            try
+                let! res = result
 
-            match res with
-            | Ok(Some value) ->
-                ctx.Response.StatusCode <- StatusCodes.Status200OK
-                do! ctx.Response.WriteAsJsonAsync(value)
-            | Ok None ->
-                let payload = {| message = "Not found" |}
-                ctx.Response.StatusCode <- StatusCodes.Status404NotFound
-                do! ctx.Response.WriteAsJsonAsync(payload)
-            | Error err ->
-                let payload = {| message = err |}
+                match res with
+                | Ok(Some value) ->
+                    ctx.Response.StatusCode <- StatusCodes.Status200OK
+                    do! ctx.Response.WriteAsJsonAsync(value)
+                | Ok None ->
+                    let payload = {| message = "Not found" |}
+                    ctx.Response.StatusCode <- StatusCodes.Status404NotFound
+                    do! ctx.Response.WriteAsJsonAsync(payload)
+                | Error err ->
+                    let payload = {| message = err |}
+                    ctx.Response.StatusCode <- StatusCodes.Status500InternalServerError
+                    do! ctx.Response.WriteAsJsonAsync(payload)
+            with ex ->
+                let payload = {| message = ex.Message; stackTrace = ex.StackTrace; hResult = ex.HResult |}
                 ctx.Response.StatusCode <- StatusCodes.Status500InternalServerError
                 do! ctx.Response.WriteAsJsonAsync(payload)
         }
