93a350f3cd6b0e2682cc54c6d6da0fbb8a2764b9
dustech <stefano.cerruti@outlook.com>
2025-04-29 16:23:33 +0200

    small images weight

--- src/Corelai.Prime.Database/migrations/V0002__init_prime_timeline.sql
+++ src/Corelai.Prime.Database/migrations/V0002__init_prime_timeline.sql
@@ -1,24 +1,24 @@
 INSERT INTO time.timeline (id, code, title, date, summary, tags, lang, version, image_path) VALUES
 ('2bf8b4d1-bccb-4fc9-8ad3-25e5a9476a55', 'CL-PRM-HIST-0000', 'ILION Constructed', '2125-03-09T12:19:00Z',
  'CORELAI SECTOR completes construction of ILION, its first capital-class interstellar vessel.',
- ARRAY['ilion', 'core-vessel', 'construction', 'sector-prime'], 'en', 1, 'timeline/0000.png'),
+ ARRAY['ilion', 'core-vessel', 'construction', 'sector-prime'], 'en', 1, 'timeline/0000.jpg'),
 
 ('09b5bbda-7093-400d-9121-25abec3571f1', 'CL-PRM-HIST-0001', 'ILION Enters Earth Orbit', '2125-03-12T06:45:00Z',
  'The flagship ILION begins orbital operations around Terra in preparation for authorization procedures.',
- ARRAY['ilion', 'orbit', 'terra', 'ueg', 'sector-prime'], 'en', 1, 'timeline/0001.png'),
+ ARRAY['ilion', 'orbit', 'terra', 'ueg', 'sector-prime'], 'en', 1, 'timeline/0001.jpg'),
 
 ('8923a1f0-6718-4067-b918-c15375a9c19c', 'CL-PRM-HIST-0002', 'Colonization Mission Authorized', '2125-03-20T13:28:00Z',
  'UEG grants CORELAI SECTOR permission to launch its first interstellar expansion mission toward TE-110D (SORAN).',
- ARRAY['authorization', 'ueg', 'mission-approval', 'soran', 'sector-prime'], 'en', 1, 'timeline/0004.png'),
+ ARRAY['authorization', 'ueg', 'mission-approval', 'soran', 'sector-prime'], 'en', 1, 'timeline/0004.jpg'),
 
 ('42229c5d-13cb-4910-b63a-4a0d68c83822', 'CL-PRM-HIST-0003', 'EMBER Constructed', '2125-04-04T13:38:00Z',
  'The EMBER battle cruiser is commissioned as an advanced tactical asset for SECTOR operations.',
- ARRAY['ember', 'core-vessel', 'construction', 'sector-prime'], 'en', 1, 'timeline/0006.png'),
+ ARRAY['ember', 'core-vessel', 'construction', 'sector-prime'], 'en', 1, 'timeline/0006.jpg'),
 
 ('31967274-f5f6-4732-99b3-5d1872df9860', 'CL-PRM-HIST-0004', 'EMBER Joins ILION Fleet', '2125-04-09T15:35:00Z',
  'EMBER enters formation with ILION, officially becoming part of CORELAI SECTOR’s expeditionary fleet.',
- ARRAY['ember', 'ilion', 'fleet-formation', 'sector-prime'], 'en', 1, 'timeline/0003.png'),
+ ARRAY['ember', 'ilion', 'fleet-formation', 'sector-prime'], 'en', 1, 'timeline/0003.jpg'),
 
 ('342c3c95-0260-4e7a-877f-aab056548653', 'CL-PRM-HIST-0005', 'Arrival at SORAN Orbit', '2125-04-20T19:45:00Z',
  'CORELAI SECTOR fleet reaches orbit of TE-110D (SORAN), initiating Phase 1 of interstellar colonization.',
- ARRAY['soran', 'arrival', 'colonization', 'expansion', 'sector-prime'], 'en', 1, 'timeline/0005.png');
+ ARRAY['soran', 'arrival', 'colonization', 'expansion', 'sector-prime'], 'en', 1, 'timeline/0005.jpg');
